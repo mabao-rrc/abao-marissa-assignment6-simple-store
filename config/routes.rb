4482a304c72f140a90b657bbe2c0f116188e8cd1
Marissa Abao <mabao@rrc.ca>
2025-07-04 00:36:30 -0500

    Add product show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
   get "/products", to: "products#index"
+  get "/products/:id", to: "products#show", as: "product"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
